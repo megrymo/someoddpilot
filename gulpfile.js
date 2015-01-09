9e55b0668deb9bd8ad9b197db223089d591d05f7
Alex Robertson <alex@someoddpilot.com>
2015-01-09 11:44:04 -0600

    Refactoring gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,22 @@ var gulp = require("gulp");
 var frontMatter = require("gulp-front-matter");
 var marked = require("gulp-marked");
 var rename = require("gulp-rename");
+var path = require("path");
 
-gulp.task("posts", function () {
+function renamePost(filePath) {
+  filePath.dirname = path.join(filePath.dirname, "/posts/", filePath.basename);
+  filePath.basename = "index";
+}
+
+function postsTask() {
   gulp.src("./src/*.md")
     .pipe(frontMatter({
       property: "frontMatter",
       remove: true
     }))
     .pipe(marked())
-    .pipe(rename(function (path) {
-      path.dirname += "/posts/" + path.basename;
-      path.basename = "index";
-    }))
+    .pipe(rename(renamePost))
     .pipe(gulp.dest("./dest"));
-});
+}
+
+gulp.task("posts", postsTask);
