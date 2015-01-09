455668afd735c1292748c3411bdad99376374aba
Alex Robertson <alex@someoddpilot.com>
2015-01-09 15:38:49 -0600

    Setting up index pages

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,18 @@ var rename = require("gulp-rename");
 var path = require("path");
 var templates = require("./templates");
 var collections = require("./collections");
+var connect = require("gulp-connect");
 
 function renamePost(filePath) {
   filePath.dirname = path.join(filePath.dirname, filePath.basename);
   filePath.basename = "index";
+}
+
+function renamePage(filePath) {
+  if (filePath.basename !== "index") {
+    filePath.dirname = path.join(filePath.dirname, filePath.basename);
+    filePath.basename = "index";
+  }
 }
 
 var postsGlob = "./src/posts/*.md";
@@ -29,15 +37,23 @@ function postsTask() {
 gulp.task("posts", postsTask);
 
 function pagesTask() {
-  return gulp.src("./src/*.md")
+  return gulp.src(["./src/*.md"])
     .pipe(collections({
       posts: "./src/posts/*.md"
     }))
     .pipe(frontMatter(fmOptions))
     .pipe(marked())
-    .pipe(rename(renamePost))
+    .pipe(rename(renamePage))
     .pipe(templates())
     .pipe(gulp.dest("./dest"));
 }
 
 gulp.task("pages", pagesTask);
+
+gulp.task("connect", function () {
+  connect.server({
+    root: "dest"
+  });
+});
+
+gulp.task("default", ["posts", "pages", "connect"]);
