31663cab73c531fcc4415b1b649b323794cd5297
Alex Robertson <alex@someoddpilot.com>
2015-01-15 13:52:39 -0600

    Using compression

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var connect = require("connect");
 var serveStatic = require("serve-static");
 var path = require("path");
+var compression = require("compression");
 
 console.log("serving");
 
 connect()
-  .use(serveStatic(path.join(__dirname, "/dest")))
+  .use(serveStatic(path.join(__dirname, "/dest")), {
+    maxAge: 1000 * 60 * 60 * 24
+  })
+  .use(compression())
   .listen(process.env.PORT || 8080);
