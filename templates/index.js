def8b73a527cd5cc5b325767f6776f1e6baf3949
Alex Robertson <alex@someoddpilot.com>
2015-01-23 16:15:58 -0600

    Refactoring templating
    Fixing for global leaks

--- templates/index.js
+++ templates/index.js
@@ -4,37 +4,6 @@ var fs = require("fs");
 var path = require("path");
 var through = require("through2");
 
-// template data globals
-var globals = {};
-
-function templateStream(file, enc, callback) {
-  function onTemplateFile(err, templateString) {
-    if (err) {
-      throw err;
-    }
-
-    var templateFn = handlebars.compile(templateString);
-
-    var data = _.extend(
-      globals,
-      file.frontMatter,
-      {
-        collections: file.collections,
-        contents: file.contents.toString()
-      }
-    );
-
-    file.contents = new Buffer(templateFn(data), "utf-8");
-    callback(null, file);
-  }
-
-  var templateName = file.frontMatter.template || "post";
-
-  var templatePath = path.join(__dirname, templateName + ".html");
-
-  fs.readFile(templatePath, "utf-8", onTemplateFile);
-}
-
 function forEachPartial(partialPath, name) {
   handlebars.registerPartial(
     name,
@@ -59,9 +28,35 @@ function templates(options) {
 
   _.forEach(options.helpers || [], forEachHelper);
 
-  globals = options.globals || {};
+  var globals = options.globals || {};
 
-  return through.obj(templateStream);
+  return through.obj(function (file, enc, callback) {
+    function onTemplateFile(err, templateString) {
+      if (err) {
+        throw err;
+      }
+
+      var templateFn = handlebars.compile(templateString);
+
+      var data = _.extend(
+        globals,
+        file.frontMatter,
+        {
+          collections: file.collections,
+          contents: file.contents.toString()
+        }
+      );
+
+      file.contents = new Buffer(templateFn(data), "utf-8");
+      callback(null, file);
+    }
+
+    var templateName = file.frontMatter.template || "post";
+
+    var templatePath = path.join(__dirname, templateName + ".html");
+
+    fs.readFile(templatePath, "utf-8", onTemplateFile);
+  });
 }
 
 module.exports = templates;
