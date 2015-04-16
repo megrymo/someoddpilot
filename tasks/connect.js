2b24f63151e750f5e2b9f0627d65d68bf9ea32eb
Alex Robertson <alex@someoddpilot.com>
2015-04-16 13:12:50 -0500

    Using gulp-live-server
    * Allows multiple static directories

--- tasks/connect.js
+++
@@ -1,8 +0,0 @@
-import gulp from "gulp";
-import connect from "gulp-connect";
-
-gulp.task("connect", function connectTask() {
-  connect.server({
-    root: "build"
-  });
-});
--- tasks/templates.js
+++
@@ -1,6 +0,0 @@
-import gulp from 'gulp';
-
-gulp.task("templates", function () {
-  return gulp.src("./src/**/*.html")
-    .pipe(gulp.dest("./build/"));
-});
